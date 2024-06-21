93e83c21101d1127d090403dc5636683fb36e0ad
DESKTOP-CSM9KCK\Welcome <cuong.nguyenkim@posco.net>
2024-06-21 09:01:25 +0700

    init first 2

--- baitap/2.go
+++ baitap/2.go
@@ -1,1 +1,21 @@
- 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int = 5;
+	expectedSum := (n * (n + 1)) / 2
+
+	var arr = []int{1,2,5,4}
+
+	sum := 0
+
+	for i := 0; i < len(arr); i++ {
+		sum += arr[i]
+	}
+
+	missingNumber := expectedSum - sum
+	fmt.Println(missingNumber)
+}
--- baitap/3.go
+++ baitap/3.go
@@ -1,1 +1,31 @@
- 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	longestRepetition := findLongestRepetition("AABBCCC")
+	fmt.Println(longestRepetition)
+}
+
+func findLongestRepetition(sequence string) int {
+	maxLength := 0
+	currentLength := 1
+
+	for i := 1; i < len(sequence); i++ {
+		if sequence[i] == sequence[i-1] {
+			currentLength++
+		} else {
+			if currentLength > maxLength {
+				maxLength = currentLength
+			}
+			currentLength = 1
+		}
+	}
+	if currentLength > maxLength {
+		maxLength = currentLength
+	}
+
+	return maxLength
+}
