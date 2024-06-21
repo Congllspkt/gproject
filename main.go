87719c02e8a4d589ee2b998b372fd24c38bebe9d
DESKTOP-CSM9KCK\Welcome <cuong.nguyenkim@posco.net>
2024-06-21 16:09:09 +0700

    init first 2

--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
-// import "gproject/baitap"
-import (
-	"net/http"
-	"github.com/gin-gonic/gin"
-  )
+import "gproject/server"
+
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-	  c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	  })
-	})
-	r.Run()
+	server.Init()
 }
 
 
